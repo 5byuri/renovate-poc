b98494ce4054e86b92cb7bcf859dc4fd470bfca4
Lars Hermges <lschwan43@gmail.com>
2026-03-18 16:07:46 +0100

    try node recommendation

--- main.go
+++ main.go
@@ -12,12 +12,10 @@ type Recommendation struct {
 }
 
 var demoRecommendations = map[string]string{
-	"@sentry/nextjs":   "9.39.0",
-	"@types/node":      "20.12.0",
-	"@types/react":     "18.2.79",
-	"@types/react-dom": "18.2.25",
-	"curl":             "7.88.1-10+deb12u9",
-	"git":              "1:2.39.5-0+deb12u3",
+	"next":      "9.6.5",
+	"node":      "20.11.1",
+	"react":     "18.7.0",
+	"react-dom": "18.3.0",
 }
 
 func recommendationHandler(w http.ResponseWriter, r *http.Request) {
@@ -66,7 +64,6 @@ func healthHandler(w http.ResponseWriter, _ *http.Request) {
 
 func main() {
 	http.HandleFunc("/recommendation", recommendationHandler)
-	http.HandleFunc("/recommendation/debian", recommendationHandler)
 	http.HandleFunc("/healthz", healthHandler)
 	log.Println("simple custom datasource demo listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
